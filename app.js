827dd05ef8db92d3ea682e224afdca48caa5f553
Olayinka Agbetanmije <olayinkaagbetanmije@gmail.com>
2022-12-09 22:25:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/GoogleDataAnalyticsCyclisticProject/Dashboard2?:language=en-US&:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/views/GoogleDataAnalyticsCyclisticProject/Dashboard2?:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
